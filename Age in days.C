55a6c3738e4952c12741c22d39fc91e090155c8c
Subro sutradhar <subroopu@gmail.com>
2025-06-11 18:18:28 +0600

    Create Age in days.C

---
+++ Age in days.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int days;
+    scanf("%d", &days);
+
+    int years = days / 365;
+    days = days % 365;
+
+    int months = days / 30;
+    days = days % 30;
+
+    printf("%d ano(s)\n", years);
+    printf("%d mes(es)\n", months);
+    printf("%d dia(s)\n", days);
+
+    return 0;
+}
