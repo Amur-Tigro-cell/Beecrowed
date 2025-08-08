fb8822700d63802f7d3e7146ca488b9c959f0394
Subro sutradhar <subroopu@gmail.com>
2025-08-08 14:01:23 +0600

    Create Sum of digits in array.C

---
+++ Sum of digits in array.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    char digits[N + 1]; 
+    scanf("%s", digits);
+
+    long long sum = 0; 
+    for (int i = 0; i < N; i++) {
+        sum += digits[i] - '0'; 
+    }
+
+    printf("%lld\n", sum);
+
+    return 0;
+}
